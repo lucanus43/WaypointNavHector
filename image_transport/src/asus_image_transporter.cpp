303677f9f573ecb1eecc7f7d1e8db7b67871cee0
Jeff Devaraj <jeffrey.devaraj@connect.qut.edu.au>
2016-11-08 17:22:23 +1000

    Changes to be committed: 	modified:   image_transport/src/asus_image_transporter.cpp  Untracked files: 	image_transport/src/asus_image_transporter.cpp~

--- image_transport/src/asus_image_transporter.cpp
+++ image_transport/src/asus_image_transporter.cpp
@@ -24,14 +24,66 @@ Date: 161108
 #include <cv_bridge/cv_bridge.h>
 
 // ------------------------- CODE ------------------------------ //
+
+// -------------------------------------------------------------
+/*
+rgbCallback - 	Called when rgb image is received. Invokes
+		feature extraction.
+
+		Derived from: http://wiki.ros.org/image_transport/Tutorials/SubscribingToImages
+
+Author: JDev 161108
+	
+*/
+// -------------------------------------------------------------
 void rgbCallback(const sensor_msgs::ImageConstPtr& msg) {
-try
-  {
-    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::waitKey(30);
+try {
+	// Show image
+	cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    	cv::waitKey(30);
+	
+	// Perform image manipulation operations
+	// ..
   }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  catch (cv_bridge::Exception& e) {
+	ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }
 }
+
+
+
+// -------------------------------------------------------------
+/*
+main -	Main execution loop
+
+Author: JDev 161108
+	
+*/
+// -------------------------------------------------------------
+int main(int argc, char **argv){
+  // Initialise ROS
+  ros::init(argc, argv, "asus_image_listener");
+  // Create node handle
+  ros::NodeHandle nh;
+  // Initialise OpenCV
+  cv::namedWindow("view");
+
+  // Local Variables
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub;
+
+  // Process
+  cv::startWindowThread();
+  sub = it.subscribe("camera/image", 1, imageCallback);
+  
+  
+  // Loop until images are no longer available or landing flag has been set.
+  // ..
+  ros::spin();
+  cv::destroyWindow("view");
+}
+
+
+
+
+
